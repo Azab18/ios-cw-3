7b5a3f68d28811f7cbaf8858bfb33f34ce1052ea
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2022-03-09 22:38:40 +0300

    done

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+        Image("wlppr")
+                .resizable()
+                .frame(width: 1000, height: 1000)
+            
+        VStack{
+        
+        Text("My favourite characters")
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+            .foregroundColor(Color.purple)
             .padding()
-    }
+            VStack{
+            Image("e")
+                .resizable()
+                .frame(width: 150, height: 150)
+            HStack{
+        Image("a")
+                .resizable()
+                .frame(width: 150, height:150)
+   
+     Image("b")
+                    .resizable()
+                    .frame(width: 150,height: 150)
+              
+            
+            
+            
+            
+            
+            
+            
+            }
 }
+    }
+    }
 
+
+
+
+
+
+
+
+
+
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.sizeCategory, .small)
     }
 }
+
+
+
+
+
+
+}
+
